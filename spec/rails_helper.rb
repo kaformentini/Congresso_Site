bc47213856e292b01ca05ae6cf509798f3ae6dd0
Kah <kaformentini@gmail.com>
2019-10-29 19:01:15 -0300

    Confurações finais doa mmbiente eu espero

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'app/channels'
+  add_filter 'app/jobs'
+  add_filter 'app/mailers'
+  add_filter 'app/helpers'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
